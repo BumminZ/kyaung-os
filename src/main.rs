f31e742dd9afaaf7c1c01cfe71a928ec7807a521
Aung Tay Zar Maung <cryptoice2001@gmail.com>
2023-11-30 02:37:43 +0630

    feat: vga buffer - part 1

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,17 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    // this function is the entry point, since the linker looks for a function named `_start` by default
-    loop {}
-}
+mod vga_buffer;
 
 // this function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    // this function is the entry point, since the linker looks for a function named `_start` by default
     loop {}
 }
